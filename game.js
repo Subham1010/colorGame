353c1697c0f687a1c308fc6946e9da11eb010cc1
Subham1010 <69982078+Subham1010@users.noreply.github.com>
2021-05-28 16:47:48 +0530

    Update game.js

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ function checkAnswer(currentLevel) {
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
           nextSequence();
-        }, 1000);
+        }, 1450);
       }
     } else {
       playSound("wrong");
